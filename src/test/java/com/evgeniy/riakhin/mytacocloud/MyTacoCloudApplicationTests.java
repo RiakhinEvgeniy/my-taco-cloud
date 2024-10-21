c412fe61fb2014bfda8abf6c5a02d89231af2d67
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-10-21 17:00:27 +0200

    sec

---
+++ src/test/java/com/evgeniy/riakhin/mytacocloud/MyTacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evgeniy.riakhin.mytacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
