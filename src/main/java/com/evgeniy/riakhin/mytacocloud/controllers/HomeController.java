6fe88cc43301f81a374e9b5de456f6adfb86be38
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-10-23 19:56:09 +0200

    ADD: HomeController. home.html. Test for HomeController

---
+++ src/main/java/com/evgeniy/riakhin/mytacocloud/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.evgeniy.riakhin.mytacocloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
