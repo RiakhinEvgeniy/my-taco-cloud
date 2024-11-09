86084c41292027cb1721f66cc753d49ad17baa21
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-11-09 13:38:44 +0100

    ADD: Validation added to fields of classes, controllers and forms.

--- src/main/java/com/evgeniy/riakhin/mytacocloud/model/Taco.java
+++ src/main/java/com/evgeniy/riakhin/mytacocloud/model/Taco.java
@@ -1,11 +1,18 @@
 package com.evgeniy.riakhin.mytacocloud.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min = 4, message = "Name must be at last 4 characters long")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at last 1 ingredient")
     private List<Ingredient> ingredients;
 }
